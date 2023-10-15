0d282601195acc26b13c7766453609f2d1a89e52
gengrui <gr1997ws@126.com>
2023-10-15 17:08:42 +0800

    11

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import (QApplication,QWidget,QMainWindow,
+                               QLabel,QScrollArea,QPushButton)
+from PySide6.QtGui import QPixmap,QImage
+
+
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    app.exec()
